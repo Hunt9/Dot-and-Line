1ed8f9379649e1cebf2b1a207039a8d5c3099a1a
Hunt9 <champthe20@gmail.com>
2019-10-21 08:17:48 +0500

    Initial Commit Done

---
+++ app/src/main/java/com/learn/dotandline/LoginActivity.java
@@ -0,0 +1,33 @@
+package com.learn.dotandline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private TextView tvRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        tvRegister = findViewById(R.id.Register);
+
+        tvRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/learn/dotandline/RegisterActivity.java
@@ -0,0 +1,35 @@
+package com.learn.dotandline;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private TextView tvLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        tvLogin = findViewById(R.id.Login);
+
+        tvLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(RegisterActivity.this,LoginActivity.class);
+                startActivity(intent);
+                finish();
+
+            }
+        });
+
+    }
+}
